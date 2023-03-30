be6e4891251e45b174cfb17250fb9f0a8512f3d1
Alyssa Samuel <83474895+Alyssa-Samuel@users.noreply.github.com>
2023-03-29 22:49:28 -0400

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,3 @@
+def at_home_workouts():
+    """Workouts to do at home without equipment."""
+    print("pushups")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Demo',
+version='0.1',
+description='A simple demonstration package',
+url='http://github.com/ontoligent/m09-demo',
+2
+author='Alyssa',
+author_email='ays8ms@virginia.edu',
+license='MIT',
+packages=['demo'])
